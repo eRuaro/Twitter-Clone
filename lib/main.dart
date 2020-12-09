3af63d37d72be504bcc514289a0155af4b3c9ecd
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2020-12-09 23:08:08 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TwitterApp());
+}
+
+class TwitterApp extends StatelessWidget {
+  TwitterApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Twitter Clone with Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+              color: Colors.blue,
+            ),
+            IconButton(
+              icon: Icon(Icons.flight),
+              onPressed: () {},
+              color: Colors.blue,
+            ),
+            IconButton(
+              icon: Icon(Icons.star),
+              onPressed: () {},
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+
+      //body, scroll view
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+
+      //Bottom navigation bar, home, search, notification, messages
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 30), label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications, size: 30), label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message, size: 30), label: ''),
+        ],
+      ),
+
+      //SideBar
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blue,
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        print('Drawer item 1 pressed');
+                      }),
+                  Text('Drawer Item 1'),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        print('Drawer item 2 pressed');
+                      }),
+                  Text('Drawer Item 2'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      endDrawer: Drawer(
+        child: Container(
+          color: Colors.blue,
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        print('Drawer item 1 pressed');
+                      }),
+                  Text('Drawer Item 1'),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        print('Drawer item 2 pressed');
+                      }),
+                  Text('Drawer Item 2'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
